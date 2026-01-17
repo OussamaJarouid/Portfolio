c8620a93b4236fb54baa576b36c775b8927beb3c
Oussama Jarouid <oussamajarouide110@gmail.com>
2026-01-17 16:59:07 +0100

    fix(vite): correct base path for Vercel

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/", // <- Mets ici le nom exact de ton repo avec les majuscules
+  base: "/", // <- Mets ici le nom exact de ton repo avec les majuscules
   plugins: [react()],
 });
