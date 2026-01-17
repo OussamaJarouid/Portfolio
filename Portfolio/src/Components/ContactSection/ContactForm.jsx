c771a121f4a8e58066c87b72d5c5ddc0c3845024
Oussama Jarouid <oussamajarouide110@gmail.com>
2026-01-17 17:05:35 +0100

    last version

--- Portfolio/src/Components/ContactSection/ContactForm.jsx
+++ Portfolio/src/Components/ContactSection/ContactForm.jsx
@@ -20,7 +20,7 @@ const ContactForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      await axios.post("/api/contact", data);
+      await axios.post("/api/app", data);
 
       toast.success("Message Sent Successfully! 🚀", {
         position: "bottom-center",
